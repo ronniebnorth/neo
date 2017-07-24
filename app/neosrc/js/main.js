3866b77e25bb694eb60c204e780cb5ffe93d1d08
Scott Means <smeans@gmail.com>
2017-07-24 13:04:02 -0400

    good progress toward demo, added first line placeholder feature

--- app/neosrc/js/main.js
+++ app/neosrc/js/main.js
@@ -1,8 +1,11 @@
 const path = require('path');
+const fs = require('fs-extra');
 
 const electron = require('electron');
 const remote = electron.remote;
 const app = remote.app;
+
+const first_lines = fs.readJsonSync('./neosrc/first_lines.json');
 
 const $ = require('jquery');
 
@@ -11,8 +14,16 @@ const neo = remote.require('./neosrc/node_modules/neo');
 const log = remote.require('./neosrc/node_modules/log.js')
 
 function showPane(id) {
-  $('#stage>div.pane').removeClass('active');
-  $('#' + id).addClass('active');
+  var hiding = $('#stage>div.pane.active');
+  if (hiding.length) {
+    document.dispatchEvent(new CustomEvent('hiding_pane', {'pane': hiding[0]}));
+
+    $('#stage>div.pane').removeClass('active');
+  }
+
+  var showing = $('#' + id);
+  document.dispatchEvent(new CustomEvent('showing_pane', {'pane': showing[0]}));
+  $(showing).addClass('active');
 }
 
 function showNextPane() {
@@ -46,12 +57,70 @@ function handleFormPost(e) {
 
 function handleNewStory(e) {
   neo.newStory();
+
+  // !!!LATER!!! just for demo
+  $('#story .author_name').text(neo.CurrentAuthor.fullName);
+
+  // Thanks to Buzzfeed:
+  // https://www.buzzfeed.com/sarahgalo/it-was-a-queer-sultry-summer?utm_term=.tm2yKX808#.tb8jzxrnr
+  var first_line = first_lines[Math.floor(Math.random() * first_lines.length)];
+  $('#story .chapter p.placeholder').html(first_line.line + '<br/>&nbsp;&nbsp;&mdash;' + first_line.attribution);
+
   showPane('story');
+
+  // !!!LATER!!! just for demo
+  selectElement($('h1.title')[0]);
+}
+
+function handleStoryKeypress(e) {
+  var focus_element = $(window.getSelection().anchorNode).closest('*');
+
+  if (e.keyCode == 13) {
+    if ($(focus_element).hasClass('replace')) {
+      $(focus_element).removeClass('replace');
+
+      var reps = $('#story .replace');
+      if (reps.length) {
+        selectElement(reps[0]);
+        e.preventDefault();
+      }
+    } else if (e.ctrlKey) {
+      var chapter = $(focus_element).closest('.chapter');
+      $(chapter).after('<div class="chapter"><p>&nbsp;</p></div>');
+      selectionToEnd($(chapter).next('.chapter').find('p')[0]);
+
+      e.preventDefault();
+    }
+  } else {
+    if ($(focus_element).hasClass('placeholder')) {
+      $(focus_element).removeClass('placeholder');
+    }
+  }
+}
+
+function selectElement(el) {
+  var range = document.createRange();
+  range.selectNodeContents(el);
+
+  var sel = window.getSelection();
+  sel.removeAllRanges();
+  sel.addRange(range);
+}
+
+function selectionToEnd(el) {
+  var range = document.createRange();
+  range.setStart(el, $(el).text().length);
+
+  var sel = window.getSelection();
+  sel.removeAllRanges();
+  sel.addRange(range);
 }
 
 $(function () {
   $(document).on('submit', handleFormPost);
   $('.new_story').on('click', handleNewStory)
+
+  $('#story').on('keypress', handleStoryKeypress);
 
   neo.StoryPane = '#story';
 
